17d28533481e80269a21565e81dec84461af0afc
amirhamza24 <ridoykhan7897890@gmail.com>
2023-10-09 16:56:31 +0600

    category section

---
+++ src/compoments/pages/Layout/Category/Category.jsx
@@ -0,0 +1,64 @@
+// import React from 'react';
+import { useEffect, useState } from 'react';
+import './Category.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import SingleCategory from './SingleCategory';
+
+const Category = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const handleBtnActive = e => {
+        fetch(`http://localhost:5000/allToy/${e}`)
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }
+
+    useEffect( () => {
+        fetch('http://localhost:5000/allToy/Sports Car')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
+    return (
+        <div className=''>
+            <div className='w-10/12 mx-auto border-t-2 border-lime-600'>
+                <div className='my-10'>
+                    <div>
+                        <h2 className='text-center text-4xl font-bold para-font mb-5'>Product Categories</h2>
+                        <p className='border'></p>
+                    </div>
+                </div>
+
+                <Tabs>
+                    <TabList>
+                        <Tab onClick={() => handleBtnActive("Sports Car")}>Sports Car</Tab>
+
+                        <Tab onClick={() => handleBtnActive("Truck")}>Truck</Tab>
+
+                        <Tab onClick={() => handleBtnActive("Mini Fire Truck")}>Fire Truck</Tab>
+                    </TabList>
+
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                </Tabs>
+
+
+                <div>
+                    {
+                        categories.map(category => <SingleCategory
+                            key={category._id}
+                            category={category}
+                        >
+
+                        </SingleCategory> )
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Category;
--- src/compoments/pages/Layout/Hom/Home.jsx
+++ src/compoments/pages/Layout/Hom/Home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 
 import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 import Deals from "../Deals/Deals";
 import Offer from "../Offer/Offer";
 
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Offer></Offer>
             <Deals></Deals>
+            <Category></Category>
         </div>
     );
 };
